e5b187d2d9189e171f1ec2c5cd05fb0f075f11b0
flaviorv <frezendevaroto@gmail.com>
2025-07-15 20:36:56 -0300

    Firing requests in a time interval when the two processors are unavailable.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
-import Redis, { Redis as RedisClient } from 'ioredis'
+import Redis from 'ioredis'
 import { Decimal } from 'decimal.js'
 
 type Payment = {
@@ -10,67 +10,47 @@ type Payment = {
 
 async function main() {
   
-  interface CustomRedis extends RedisClient {
-    dedupCheck: (key: string, value: string) => Promise<number>
-  }
-  const redis = new Redis('redis://redis:6379') as CustomRedis
-  redis.defineCommand('dedupCheck', {
-    numberOfKeys: 1,
-    lua: `
-      if redis.call("SISMEMBER", KEYS[1], ARGV[1]) == 1 then
-          return 0
-      else
-          redis.call("SADD", KEYS[1], ARGV[1])
-          return 1
-      end
-    `
-  })
-
+  const redis = new Redis('redis://redis:6379')
   const app = new Hono()
 
-  app.post('/payments', async (c) => {
-    const body = await c.req.json<Payment>()
+  async function processPayment(processor:string, payment: Payment) {
     const currentDate = new Date()
-    const dedupResult = await redis.dedupCheck('payments:processed', body.correlationId)
-    if (dedupResult === 0) {
-      return c.text('Duplicate payment', 409)
-    }
-    const response = await fetch('http://payment-processor-default:8080/payments', {
+    const response = await fetch(`http://payment-processor-${processor}:8080/payments`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        correlationId: body.correlationId,
-        amount: body.amount,
-        requestedAt: currentDate.toISOString(),
-      }),
-    })
-    if (!response.ok) {
-      const fallbackResponse = await fetch('http://payment-processor-fallback:8080/payments', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          correlationId: body.correlationId,
-          amount: body.amount,
-          requestedAt: currentDate.toISOString(),
-        })
+        correlationId: payment.correlationId,
+        amount: payment.amount,
+        requestedAt: currentDate.toISOString()
       })
-      if (!fallbackResponse.ok) {
-          return c.text('Failed to fetch from external service', 502)
-      }
-      await redis.zadd('payment:fallback', currentDate.getTime(), `${body.amount}:${body.correlationId}`)
-      return fallbackResponse
+    })
+    if (response.ok) {
+      await redis.zadd(`payment:${processor}`, currentDate.getTime(), `${payment.correlationId}:${payment.amount}`)
     }
-    await redis.zadd('payment:default', currentDate.getTime(), `${body.amount}:${body.correlationId}`)    
     return response
-  })
-
-  app.put('/admin/configurations/failure', async(c) => {
-    const response = fetch('http://payment-processor-default:8080/admin/configurations/failure', {
-      method:'PUT',
-      headers: {'Content-Type': 'application/json', 'X-Rinha-Token': '123'},
-      body: JSON.stringify({failure: true})
-    }) 
-    return response
+  }
+  const PROCESSORS = ['default', 'fallback']
+  app.post('/payments', async (c) => {
+    const body = await c.req.json<Payment>();
+    for (const processor of PROCESSORS) {
+      const res = await processPayment('default', body);
+      if (res.ok) {
+        return res
+      }
+    }
+    const MAX_WAIT_MS = 5000
+    const INTERVAL_MS = 100
+    let elapsed = 0
+    while (elapsed < MAX_WAIT_MS) {
+      for (const processor of PROCESSORS) {
+        const res = await processPayment(processor, body)
+        if (res.ok) {
+          return res
+        }
+      }
+      await new Promise((res) => setTimeout(res, INTERVAL_MS))
+      elapsed += INTERVAL_MS
+    }
   })
 
   app.get('/payments-summary', async (c) => {
@@ -82,10 +62,10 @@ async function main() {
     if (isNaN(fromTimestamp) || isNaN(toTimestamp)) return c.text('Invalid date format', 400)
     const defaultPayments = await redis.zrangebyscore('payment:default', fromTimestamp, toTimestamp)
     const totalRequestsDefault = defaultPayments.length
-    const totalAmountDefault = defaultPayments.reduce((sum: Decimal, val: string) => sum.plus(Decimal(val.split(':')[0])), new Decimal(0))
+    const totalAmountDefault = defaultPayments.reduce((sum: Decimal, val: string) => sum.plus(Decimal(val.split(':')[1])), new Decimal(0))
     const fallbackPayments = await redis.zrangebyscore('payment:fallback', fromTimestamp, toTimestamp)
     const totalRequestsFallback = fallbackPayments.length
-    const totalAmountFallback = fallbackPayments.reduce((sum: Decimal, val: string) => sum.plus(Decimal(val.split(':')[0])), new Decimal(0))
+    const totalAmountFallback = fallbackPayments.reduce((sum: Decimal, val: string) => sum.plus(Decimal(val.split(':')[1])), new Decimal(0))
     return c.json({
       default: {
         totalRequests: totalRequestsDefault,
@@ -128,11 +108,33 @@ async function main() {
     return new Response(null, { status: 200 })
   })
 
-  app.get('/status', async () => {
-    return fetch('http://payment-processor-default:8080/payments/service-health')
+  app.put('/admin/configurations/failure', async(c) => {
+    const rawBody = await c.req.text()
+    const defaultResponse = await fetch('http://payment-processor-default:8080/admin/configurations/failure', {
+      method:'PUT',
+      headers: {'Content-Type': 'application/json', 'X-Rinha-Token': '123'},
+      body: rawBody
+    }) 
+    const fallbackResponse = await fetch('http://payment-processor-fallback:8080/admin/configurations/failure', {
+      method:'PUT',
+      headers: {'Content-Type': 'application/json', 'X-Rinha-Token': '123'},
+      body: rawBody
+    }) 
+    const defaultBody = await defaultResponse.json()
+    const fallbackBody = await fallbackResponse.json()
+    return new Response(JSON.stringify({defaultBody, fallbackBody}), {status: defaultResponse.status})
   })
+
+  app.get('/status', async () => {
+    const defaultResponse = await fetch('http://payment-processor-default:8080/payments/service-health')
+    const fallbackResponse = await fetch('http://payment-processor-default:8080/payments/service-health')
+    const defaultBody = await defaultResponse.json()
+    const fallbackBody = await fallbackResponse.json()
+    return new Response(JSON.stringify({defaultBody, fallbackBody}), {status: defaultResponse.status})
+  })
+
   serve({ fetch: app.fetch, port: 3000, hostname: '0.0.0.0' })
-  console.log('Hono server running at http://localhost:3000')
+  console.log('Hono server running at http://localhost:9999')
 }
 
 main().catch((err) => {
